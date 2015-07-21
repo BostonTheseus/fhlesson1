76c6d344d277ac8855157ef3c05902da678e1cc8
Robert Morris <obviouslyrob@gmail.com>
2015-07-21 23:21:36 +0000

    Added factorygirl gem and updated testfile to disable fixtures

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'sass-rails', '>= 3.2'
 
 gem 'simple_form'
 
+gem "factory_girl_rails", "~> 4.0"
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
 	test "unique_tag" do
+		quote = FactoryGirl.create(:quote)
+		get :show, :id => quote.id
 		quote = Quote.create(:author => 'James Joyce', :saying => 'Yolo!')
 		expected = 'JJ#' + quote.id.to_s
 		actual = quote.unique_tag
